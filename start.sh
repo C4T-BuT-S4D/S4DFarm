5b6c758f6699e6303b7de04c35c93c1ed5338737
pomo-mondreganto <1728390Roman>
2018-10-21 20:42:13 +0300

    Gunicorn logging removed icons changed

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 source destructive_farm_venv/bin/activate
-gunicorn --worker-class gevent --worker-connections 768 --bind 0.0.0.0:5000 --reload --access-logfile logs/access.log --error-logfile logs/error.log server:app
+gunicorn --worker-class gevent --worker-connections 768 --bind 0.0.0.0:5000 --reload server:app
